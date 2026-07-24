74fb160d13a1aa7db815e9aa9ef179293afb84eb
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-07-24 15:27:39 +0530

    Add files via upload

---
+++ CreateClassMethodObject/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.display("Chandan",25);
+
+    }
+}
---
+++ CreateClassMethodObject/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    String name;
+    int age;
+
+    public void display(String name, int age){
+        System.out.println("Name :"+name);
+        System.out.println("Age  :"+age);
+    }
+}
