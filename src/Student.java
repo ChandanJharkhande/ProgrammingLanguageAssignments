437b04853b7fa9c8edcac3137790429ecaa13f0a
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-07-24 15:28:31 +0530

    Delete src directory

--- src/Student.java
+++
@@ -1,9 +0,0 @@
-public class Student {
-    String name;
-    int age;
-
-    public void display(String name, int age){
-        System.out.println("Name :"+name);
-        System.out.println("Age  :"+age);
-    }
-}
