aa0742453931f6e3b3e4ae8a63d25d12ae647db8
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-07-25 14:41:21 +0530

    Add Main class with data type examples
    Implement a simple Java program demonstrating various data types.

---
+++ DiffDataTypes/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 10;
+        boolean isJavaFun = true;
+        char grade = 'A';
+        float price = 99.99f;
+        double p1 = 3.14159265359;
+
+
+        System.out.println("Integer value :"+number);
+        System.out.println("Boolean value :"+isJavaFun);
+        System.out.println("Character value :"+grade);
+        System.out.println("Float value :"+price);
+        System.out.println("Double value :"+p1);
+
+    }
+}
