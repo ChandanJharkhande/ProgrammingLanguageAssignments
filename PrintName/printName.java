e1e8d3756539bf3085ce35decc0e745e5b2600fc
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-07-25 15:12:49 +0530

    Add PrintName class with printName method

---
+++ PrintName/printName.java
@@ -0,0 +1,12 @@
+public class PrintName {
+    static void printName(){
+
+        System.out.println("My name is Chandan Jharkhande");
+
+    }
+
+
+    public static void main(String[]args){
+       printName();
+    }
+}
