8b0f1b59b88a89dc3f05d588963bb3830dcc7817
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-07-26 11:42:38 +0530

    Equal Not Equal operation

---
+++ EqualNotEqualOpertion/EqualNotEqual.java
@@ -0,0 +1,21 @@
+public class EqualNotEqual {
+    static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        if(a==b){
+            System.out.println("a is eual to b");
+        }else {
+            System.out.println("a is not eual to b");
+        }
+
+        if(a != b){
+            System.out.println("a is not equal to be");
+        }else {
+            System.out.println("a is equal to be");
+        }
+
+
+
+    }
+}
