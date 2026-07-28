84cf394ebd8ec3d6f834be92a859d731f6f8c45b
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-07-28 16:36:52 +0530

    Add EvenNumbers class to print even numbers

---
+++ EvenNumbers/EvenNumbers.java
@@ -0,0 +1,11 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int i = 10;
+
+        while (i<=100){
+            System.out.println(i);
+            i = i+2;
+        }
+
+    }
+}
