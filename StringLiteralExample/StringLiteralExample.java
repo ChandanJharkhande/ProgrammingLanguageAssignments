860823177e1d282c930bb0d32c0b4440915ff61d
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-03 19:53:37 +0530

    Different ways creating a string

---
+++ StringLiteralExample/StringLiteralExample.java
@@ -0,0 +1,11 @@
+public class StringLiteralExample {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+        System.out.println(str1 == str2); // true (same object)
+    }
+}
