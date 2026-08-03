f8fb6fb3fb2a9e02780ad1f717358ffaab9ec4a7
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-03 20:08:06 +0530

    Different ways creating a string

---
+++ StringLiteralExample/StringBuilderExample.java
@@ -0,0 +1,8 @@
+public class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        String str = sb.toString();
+
+        System.out.println(str);
+    }
+}
