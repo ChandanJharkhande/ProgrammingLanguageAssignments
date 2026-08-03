a7dc8b959e5ed218ef5be4481947ecc97265c1c9
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-03 20:05:23 +0530

    Different ways creating a string

---
+++ StringLiteralExample/ByteArrayExample.java
@@ -0,0 +1,8 @@
+public class ByteArrayExample {
+    public static void main(String[] args) {
+        byte[] b = {72, 101, 108, 108, 111};
+        String str = new String(b);
+
+        System.out.println(str);
+    }
+}
