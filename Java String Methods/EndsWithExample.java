98ebf466a8aa8669ec05daf02bbe4340d0bd910a
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 15:14:45 +0530

    equalsIgnoreCase(), startsWith(), endsWith() and compareTo()

---
+++ Java String Methods/EndsWithExample.java
@@ -0,0 +1,8 @@
+public class EndsWithExample {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+
+        System.out.println(str.endsWith("Programming"));
+        System.out.println(str.endsWith("Java"));
+    }
+}
