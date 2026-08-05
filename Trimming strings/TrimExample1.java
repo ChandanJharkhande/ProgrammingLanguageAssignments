247ba656e68c52acaecdd4a30e482bb495d09459
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 15:23:37 +0530

    Trimming strings with trim()

---
+++ Trimming strings/TrimExample1.java
@@ -0,0 +1,8 @@
+public class TrimExample1 {
+    public static void main(String[] args) {
+        String str = "   Hello Java   ";
+
+        System.out.println("Before trim: '" + str + "'");
+        System.out.println("After trim: '" + str.trim() + "'");
+    }
+}
