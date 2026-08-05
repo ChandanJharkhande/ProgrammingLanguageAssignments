db4e956c800c309fb1275b68ff3d8a59cf4b3481
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 15:38:36 +0530

    Replacing characters in strings with replace()

---
+++ Replacing characters in strings/ReplaceExample.java
@@ -0,0 +1,7 @@
+public class ReplaceExample {
+    public static void main(String[] args) {
+        String str = "apple apple apple";
+
+        System.out.println(str.replace("apple", "mango"));
+    }
+}
