fd20ae4e98dc072eeb620078134241c5807e99be
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 15:40:06 +0530

    Replacing characters in strings with replace()

---
+++ Replacing characters in strings/ReplaceSpaces.java
@@ -0,0 +1,9 @@
+public class ReplaceSpaces {
+    public static void main(String[] args) {
+        String str = "Java Full Stack";
+
+        String result = str.replace(' ', '-');
+
+        System.out.println(result);
+    }
+}
