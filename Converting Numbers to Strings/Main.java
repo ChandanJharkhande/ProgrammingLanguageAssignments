40516e7d761c58950c4ba275f2295b354a716c1f
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 15:57:39 +0530

    Converting Numbers to Strings with valueOf()

---
+++ Converting Numbers to Strings/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 123;
+
+        String str = String.valueOf(number);
+
+        System.out.println("Number: " + number);
+        System.out.println("String: " + str);
+    }
+}
