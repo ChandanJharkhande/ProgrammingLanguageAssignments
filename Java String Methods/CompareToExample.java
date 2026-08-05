f9b71b08ded4deccd903b6583cc20759188e4d95
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 15:18:40 +0530

    equalsIgnoreCase(), startsWith(), endsWith() and compareTo()

---
+++ Java String Methods/CompareToExample.java
@@ -0,0 +1,12 @@
+public class CompareToExample {
+    public static void main(String[] args) {
+        String s1 = "Apple";
+        String s2 = "Apple";
+        String s3 = "Banana";
+        String s4 = "Ant";
+
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareTo(s4));
+    }
+}
