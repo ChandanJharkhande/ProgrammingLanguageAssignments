23d71be0b8c8d05cdecbc34505de336fccaee5cb
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 16:13:02 +0530

    Converting to uppercase and lowercase
    Implement a simple Java program to demonstrate string case conversion.

---
+++ Converting to uppercase and lowercase/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+
+        System.out.println("Original  : " + str);
+        System.out.println("Uppercase : " + str.toUpperCase());
+        System.out.println("Lowercase : " + str.toLowerCase());
+    }
+}
