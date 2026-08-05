b6f1d9f866be5ea728e373c5f35dbab9b5b4bb41
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 15:59:01 +0530

    Converting Numbers to Strings with valueOf()

---
+++ Converting Numbers to Strings/Main2.java
@@ -0,0 +1,9 @@
+public class Main2 {
+    public static void main(String[] args) {
+        double price = 99.99;
+
+        String str = String.valueOf(price);
+
+        System.out.println(str);
+    }
+}a
