5926be9762210fadc55d4a3c6b8d76dabc8b81cc
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 16:09:09 +0530

    Converting integer objects to Strings

---
+++ Converting integer objects to Strings/Main4.java
@@ -0,0 +1,9 @@
+public class Main4 {
+    public static void main(String[] args) {
+        Integer num = 400;
+
+        String str = String.format("%d", num);
+
+        System.out.println(str);
+    }
+}
