32b98ed043d40a67a793ef5a9cdfd2c8969b0182
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 15:35:33 +0530

    Replacing characters in strings with replace()

---
+++ Replacing characters in strings/ReplaceWord.java
@@ -0,0 +1,9 @@
+public class ReplaceWord {
+    public static void main(String[] args) {
+        String str = "I like Java";
+
+        String result = str.replace("Java", "Python");
+
+        System.out.println(result);
+    }
+}
