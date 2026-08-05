5b87b3d91a1c199dd309156de1060cda74567576
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 15:32:23 +0530

    Replacing characters in strings with replace()

---
+++ Replacing characters in strings/ReplaceCharacter.java
@@ -0,0 +1,10 @@
+public class ReplaceCharacter {
+    public static void main(String[] args) {
+        String str = "banana";
+
+        String result = str.replace('a', 'o');
+
+        System.out.println("Original String: " + str);
+        System.out.println("New String: " + result);
+    }
+}
