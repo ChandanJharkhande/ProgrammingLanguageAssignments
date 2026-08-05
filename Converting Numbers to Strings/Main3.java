4912767cd7d465cc5d4b7dfc2bdc7c649960960c
CHANDAN JHARKHANDE <57889212+ChandanJharkhande@users.noreply.github.com>
2026-08-05 16:01:20 +0530

    Converting Numbers to Strings with valueOf()

---
+++ Converting Numbers to Strings/Main3.java
@@ -0,0 +1,13 @@
+public class Main3 {
+    public static void main(String[] args) {
+        int a = 10;
+        long b = 100000L;
+        float c = 12.5f;
+        double d = 45.67;
+
+        System.out.println(String.valueOf(a));
+        System.out.println(String.valueOf(b));
+        System.out.println(String.valueOf(c));
+        System.out.println(String.valueOf(d));
+    }
+}
